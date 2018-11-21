c17b31a6e553a4307569266dca3d042287310220
Fr0sT-Brutal <fr0st.brutal@gmail.com>
2018-11-21 17:06:19 +0300

    Support x64 target in build script. Build fails both for x64 and x32

--- build.bat
+++ build.bat
@@ -1,20 +1,31 @@
 :: Script to build 7Zip using VS 10.0
 ::   %1 - [optional] if == "rebuild", a full rebuild is executed
+::   %2 - [optional] if == "x64", will build binaries for x64 arch
 
 @ECHO OFF
 
 SETLOCAL
 
-SET VCVars=C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat
+SET VCVars=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
 
-CALL "%VCVars%" || GOTO :Err
+:: ~~ Run ~~
+
 CD CPP\7zip || GOTO :Err
 :: Cleanup
-IF (.%1.==.rebuild.) (
+IF .%1.==.rebuild. (
 	DEL /Q /S *.exe *.dll *.obj *.lib *.exp *.res *.pch *.asm || GOTO :Err
+	SHIFT
 )
+
 :: Build
-CALL nmake NEW_COMPILER=1 MY_STATIC_LINK=1 || GOTO :Err
+IF .%1.==.x64. (
+	SET Nmake_CPU=CPU=AMD64
+	SET VCVarsCPU=amd64
+)
+
+CALL "%VCVars%" %VCVarsCPU% || GOTO :Err
+
+CALL nmake %CPU% NEW_COMPILER=1 MY_STATIC_LINK=1 || GOTO :Err
 
 GOTO :EOF
 
